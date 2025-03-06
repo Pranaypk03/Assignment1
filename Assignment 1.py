88e9bfbe9e2251299000caea0fee977211492e69
Pranay Kumar <103020902+Pranaypk03@users.noreply.github.com>
2025-03-06 15:08:55 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+#Perform Basic Mathematical Operations
+a = int(input("enter the first number: "))
+b = int(input('enter the second number: '))
+#addition
+sum = a+b
+print('Addition: ',sum)
+#Subtraction
+sub = a-b
+print('Subtraction: ',sub)
+#Multiplication
+mul = a*b
+print('Multiplication: ',mul)
+#Division
+div = a/b
+print('Division: ',div)
---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+firstname = input("enter the first name: ")
+lastname = input("enter the last name: ")
+
+name = firstname +' '+ lastname
+print(f"Hello, {name}! Welcome to the Python program.")
